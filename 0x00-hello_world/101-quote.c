735f43a638c50f4e5755e481efa299fb9ac0f64f
Ofentsee <ofentselebelo11@gmail.com>
2023-06-17 01:48:59 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,10 +1,11 @@
 #include <stdio.h>
 #include <unistd.h>
 /**
- * main - print a test on standard output
+ * main - print a script on standard output
  * Return: 1(success)
  */
 int main(void)
 {
 	in("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
 	return (1);
+}
