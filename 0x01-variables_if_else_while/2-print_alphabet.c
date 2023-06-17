fa373a8225744b2e475889d7b65acc3f85fd13e2
Ofentsee <ofentselebelo11@gmail.com>
2023-06-17 12:02:03 -0700

    2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main -n prints the alphabet
+ * Return: Always 0 (succes)
+ */
+{
+	char alp[26] = "abcdefghijklmnopqrstuvwxyz";
+	int i;
+
+	for (i = 0; i < 26; i++)
+	{
+		putchar(alp[i]);
+	}
+	putchar('\n');
+	return (0);
+}
