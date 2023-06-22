2df5b97851c2404f2ec43cc5d30bc3336cb86cb4
Ofentsee <ofentselebelo11@gmail.com>
2023-06-22 07:01:52 -0700

    ./101-print_numbers

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -1,25 +1,27 @@
 #include "main.h"
+
 /**
- * print_number - printsan integer
+ * print_number - prints an integer
  * @n: integer to be printed
  */
-void orint_number(int n)
+void print_number(int n)
 {
-	unsigned int n1;
+unsigned int n1;
 
-	if (n < 0)
-	{
-	n1 = -n;
-	_putchar('_');
-	}
-	else
-	{
-	n1 = n;
-	}
+if (n < 0)
+{
+n1 = -n;
+_putchar('-');
+}
+else
+{
+n1 = n;
+}
 
-	if (n1 / 10)
-	{
-	print_number(n1 / 10);
-	}
-	_putchar((n1 % 10) + '0');
+if (n1 / 10)
+{
+print_number(n1 / 10);
+}
+
+_putchar((n1 % 10) + '0');
 }
