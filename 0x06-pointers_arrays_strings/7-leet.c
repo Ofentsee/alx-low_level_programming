77bd3cf3b6253f135275f4873f28849ac53d6782
Ofentsee <ofentselebelo11@gmail.com>
2023-06-29 09:45:46 -0700

    7-1337

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -1,16 +1,16 @@
 #include "main.h"
 /**
- * leet -  function that encodes a string into 1337
+ * leet - encode into 1337speak
  * @n: input value
  * Return: n value
  */
-char *leet(char *)
+char *leet(char *n)
 {
 	int i, j;
 	char s1[] = "aAeEoOtTlL";
 	char s2[] = "4433007711";
 
-	for (i = 0; n[i] != '\0' i++)
+	for (i = 0; n[i] != '\0'; i++)
 	{
 		for (j = 0; j < 10; j++)
 		{
